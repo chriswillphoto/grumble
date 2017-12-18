20a397e23dae4325a9fd9232f0c4c68455192f40
Sam Cordingley <sam@cordandberg.com>
2017-12-18 11:32:58 +1100

    homepage component

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { PureComponent as Component } from 'react';
+
+
+
+class Home extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Grumble</h1>
+
+      </div>
+    );
+  }
+}
+
+export default Home;
