fddba4fff8c4bcc69d7279acc5d7f6118a1a235b
chriswillphoto <christophernekich@gmail.com>
2017-12-19 12:43:10 +1100

    halfway through login things

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { PureComponent as Component } from 'react';
 import Searchbar from './Searchbar';
 import Restaurantviewer from './Restaurantviewer';
+import Login from './Login'
 import { Link } from 'react-router-dom';
 import axios from 'axios'
 import Restaurant from './Restaurant'
@@ -14,21 +15,40 @@ class Home extends Component {
       rests: [
         {id: 1, suburb: "bondi", name: "whatever"}, {id: 2, suburb: "bondi", name: "test"}, {id: 3, suburb: "chatswood", name: "dno man"}
       ],
-      matched: null
+      matched: null,
+      loggedIn: false,
     }
 
     this.qHandle = this.qHandle.bind(this)
+
     axios.get("http://localhost:5000/restaurants").then(res => {
       this.setState({rests: res.data})
     })
 
   };
 
+  // componentDidMount(){
+  //       console.log(this.state.rests)
+  // }
+
   qHandle(e){
     // console.log(e)
     this.setState({suburb: e.suburb});
-    const filtered = this.state.rests.filter(rest => rest.suburb === e.suburb)
-    this.setState({matched: filtered})
+    const filtered = this.state.rests.filter(rest => rest.suburb.indexOf(e.suburb) !== -1 )
+    if(filtered.length === 0){
+      this.setState({matched: null})
+    }else{
+      this.setState({matched: filtered})
+    }
+  }
+
+  loginHandler(details){
+    const send = {email: details.email, password: details.password}
+    axios.post("http://localhost:5000/login", send).then(res => {
+      sessionStorage.setItem("token", res.data.auth_token)
+    })
+
+
   }
 
   yes(e){
@@ -68,9 +88,10 @@ class Home extends Component {
   render() {
     return(
       <div>
+        <Login loginform={(i) => this.loginHandler(i)}/>
         <h1 className="siteHeader">Grumble</h1>
         <Searchbar query={(state) => { this.qHandle(state) }}/>
-        {this.state.matched ? <Restaurantviewer matched={this.state.matched[0]} button={(e) => {this.yes(e)} } /> : ""}
+        {this.state.matched ? <Restaurantviewer matched={this.state.matched[0]} button={(e) => {this.yes(e)} } /> : "Please Enter A Sydney Suburb"}
       </div>
     );
   }
