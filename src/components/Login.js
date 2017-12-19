bc7735fce59de56eb0d8acbfb09be524ae38eef0
chriswillphoto <christophernekich@gmail.com>
2017-12-20 08:26:28 +1100

    login box now shows hides appropriately and is somewhat style right

--- src/components/Login.js
+++ src/components/Login.js
@@ -19,13 +19,13 @@ class Login extends Component {
 
   render(){
     return(
-    <div>
+    <div className="login-form">
         <form onSubmit={(event) => {this.submitHandle(event)} }>
           <label>Email: </label>
-          <input type="text" onChange={ (event) => {this.setState({email: event.target.value})} } value={this.state.email} ></input>
+          <input id="email" type="text" onChange={ (event) => {this.setState({email: event.target.value})} } value={this.state.email} ></input>
           <label>Password: </label>
           <input type="password" onChange={ (event) => {this.setState({password: event.target.value})} } value={this.state.password}></input>
-          <button>Yes</button>
+          <button id="login-button">Login</button>
         </form>
     </div>
     )
